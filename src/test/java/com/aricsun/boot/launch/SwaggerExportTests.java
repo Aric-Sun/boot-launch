f89ef0e32e3ee8371fbf77cf71e2b803fbfcf494
Aric-Sun <zsunqian@foxmail.com>
2020-12-08 00:52:06 +0800

    从Swagger到处离线文档，以HTML为例

---
+++ src/test/java/com/aricsun/boot/launch/SwaggerExportTests.java
@@ -0,0 +1,41 @@
+package com.aricsun.boot.launch;
+
+import io.github.swagger2markup.GroupBy;
+import io.github.swagger2markup.Language;
+import io.github.swagger2markup.Swagger2MarkupConfig;
+import io.github.swagger2markup.Swagger2MarkupConverter;
+import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+import io.github.swagger2markup.markup.builder.MarkupLanguage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.net.URL;
+import java.nio.file.Paths;
+
+/**
+ * @author AricSun
+ * @date 2020.12.07 21:40
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)  // 就是application.yml的8888
+public class SwaggerExportTests {
+
+    @Test
+    public void generateAsciiDocs() throws Exception{
+        // 输出Ascii格式
+        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+                .withMarkupLanguage(MarkupLanguage.MARKDOWN)  // 设置生成格式（ASCIIDOC/MARKDOWN）
+                .withOutputLanguage(Language.ZH)  // 自然语言类型，ZH：中文
+                .withPathsGroupedBy(GroupBy.TAGS)
+                .withGeneratedExamples()
+                .withoutInlineSchema()
+                .build();
+
+        Swagger2MarkupConverter.from(new URL("http://localhost:8888/v2/api-docs"))
+                .withConfig(config)
+                .build()
+                .toFile(Paths.get("src/main/resources/docs/asciidoc"));
+    }
+}
