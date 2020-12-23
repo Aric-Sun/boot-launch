c87eb4c7e47a6a2f26bf7faa975eb8eafdbd4f36
Aric-Sun <zsunqian@foxmail.com>
2020-12-24 01:14:17 +0800

    P52 5.7.thymeleaf内置对象与工具类

--- src/main/java/com/aricsun/boot/launch/controller/TemplateController.java
+++ src/main/java/com/aricsun/boot/launch/controller/TemplateController.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -21,9 +23,15 @@ public class TemplateController {
     ArticleService articleService;
 
     @GetMapping("/thymeleaf")
-    public String index(String name, Model model){
+    public String index(@RequestParam String name1,
+                        HttpServletRequest request,
+                        Model model){
         List<ArticleVO> articles = articleService.getAll();
         model.addAttribute("articles", articles);
+
+        request.setAttribute("name2","curry");
+        request.getSession().setAttribute("name3","james");
+        request.getServletContext().setAttribute("name4","jordan");
 
         // 模板名称，实际的目录为：src/main/resources/templates/thymeleaftemp.html
         return "thymeleaftemp";
